df43cc636b0e0877612769410315901bccbff2db
ajs418 <ajs418@uclive.ac.nz>
2023-09-22 11:03:18 +1200

    Create a basic kotlin party class with room repo

---
+++ Quickfire/app/src/main/java/nz/ac/uclive/ajs418/quickfire/Party.kt
@@ -0,0 +1,13 @@
+package nz.ac.uclive.ajs418.quickfire
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "party")
+class Party (
+    @ColumnInfo var name: String,
+    @ColumnInfo var members: List<User>
+        ) {
+    @PrimaryKey(autoGenerate = true) var id: Long = 0
+}
