d9464f61597431c34ebd775ef4e13ccbd7610198
ajs418 <ajs418@uclive.ac.nz>
2023-09-28 20:58:28 +1300

    Add media table to the database

--- Quickfire/app/src/main/java/nz/ac/uclive/ajs418/quickfire/database/QuickfireDatabase.kt
+++ Quickfire/app/src/main/java/nz/ac/uclive/ajs418/quickfire/database/QuickfireDatabase.kt
@@ -4,14 +4,21 @@ import nz.ac.uclive.ajs418.quickfire.dao.LikeDao
 import nz.ac.uclive.ajs418.quickfire.entity.Like
 import android.content.Context
 import androidx.room.*
+import nz.ac.uclive.ajs418.quickfire.dao.MediaDao
+import nz.ac.uclive.ajs418.quickfire.dao.PartyDao
+import nz.ac.uclive.ajs418.quickfire.dao.UserDao
 import nz.ac.uclive.ajs418.quickfire.entity.Converters
+import nz.ac.uclive.ajs418.quickfire.entity.Media
 import nz.ac.uclive.ajs418.quickfire.entity.Party
 import nz.ac.uclive.ajs418.quickfire.entity.User
 
-@Database(entities =  [User::class, Like::class, Party::class], version = 1)
+@Database(entities =  [User::class, Like::class, Party::class, Media::class], version = 1)
 @TypeConverters(Converters::class)
 abstract class QuickfireDatabase : RoomDatabase() {
+    abstract fun userDao() : UserDao
     abstract fun likeDao() : LikeDao
+    abstract fun partyDao() : PartyDao
+    abstract fun mediaDao() : MediaDao
 
     companion object {
         @Volatile
