2f749140af484fb5afd3754c685dbe1fc72a7f3d
ajs418 <ajs418@uclive.ac.nz>
2023-09-28 20:53:22 +1300

    Create the media repo

--- Quickfire/app/src/main/java/nz/ac/uclive/ajs418/quickfire/repository/MediaRepository.kt
+++ Quickfire/app/src/main/java/nz/ac/uclive/ajs418/quickfire/repository/MediaRepository.kt
@@ -1,5 +1,17 @@
 package nz.ac.uclive.ajs418.quickfire.repository
 
-class MediaRepository {
+import androidx.annotation.WorkerThread
+import nz.ac.uclive.ajs418.quickfire.dao.MediaDao
+import nz.ac.uclive.ajs418.quickfire.entity.Media
+import kotlinx.coroutines.flow.Flow
+
+class MediaRepository (private val mediaDao: MediaDao){
+    val media: Flow<List<Media>> = mediaDao.getAll()
+
+    @Suppress("RedundantSuspendModifier")
+    @WorkerThread
+    suspend fun insert(media: Media) {
+        mediaDao.insert(media)
+    }
 
 }
