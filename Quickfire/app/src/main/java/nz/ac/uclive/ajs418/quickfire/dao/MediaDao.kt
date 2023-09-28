c066cb7d1d357e551e2e2d43cff628842e6ce8d5
ajs418 <ajs418@uclive.ac.nz>
2023-09-28 20:51:21 +1300

    Create the media Dao

--- Quickfire/app/src/main/java/nz/ac/uclive/ajs418/quickfire/dao/MediaDao.kt
+++ Quickfire/app/src/main/java/nz/ac/uclive/ajs418/quickfire/dao/MediaDao.kt
@@ -1,8 +1,18 @@
 package nz.ac.uclive.ajs418.quickfire.dao
 
 import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import kotlinx.coroutines.flow.Flow
+import nz.ac.uclive.ajs418.quickfire.entity.Media
 
 @Dao
 interface MediaDao {
+
+    @Insert
+    suspend fun insert(media : Media): Long
+
+    @Query("SELECT * FROM media")
+    fun getAll(): Flow<List<Media>>
 
 }
