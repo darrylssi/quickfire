f90b2cd8f6818bc3ea6d05be7a0b11a1f6298b92
ajs418 <ajs418@uclive.ac.nz>
2023-10-14 20:04:40 +1300

    Create party view model

---
+++ Quickfire/app/src/main/java/nz/ac/uclive/ajs418/quickfire/viewmodel/PartyViewModel.kt
@@ -0,0 +1,30 @@
+package nz.ac.uclive.ajs418.quickfire.viewmodel
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.asLiveData
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
+import nz.ac.uclive.ajs418.quickfire.entity.Party
+import nz.ac.uclive.ajs418.quickfire.repository.PartyRepository
+import java.lang.IllegalArgumentException
+
+class PartyViewModel(private val partyRepository: PartyRepository) : ViewModel() {
+    val parties: LiveData<List<Party>> = partyRepository.parties.asLiveData()
+
+    fun addParty(party: Party) = viewModelScope.launch {
+        partyRepository.insert(party)
+    }
+
+}
+
+class PartyViewModelFactory(private val repository: PartyRepository) : ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if(modelClass.isAssignableFrom(PartyViewModel::class.java)) {
+            @Suppress("UNCHECKED_CAST")
+            return PartyViewModel(repository) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
+}
