2f8fd0b6364bc83c50577286496fed69a7d8a394
ajs418 <ajs418@uclive.ac.nz>
2023-10-15 15:12:07 +1300

    Add view models to Connect/Server play frags

--- Quickfire/app/src/main/java/nz/ac/uclive/ajs418/quickfire/fragments/ClientPlayFragment.kt
+++ Quickfire/app/src/main/java/nz/ac/uclive/ajs418/quickfire/fragments/ClientPlayFragment.kt
@@ -1,5 +1,6 @@
 package nz.ac.uclive.ajs418.quickfire.fragments
 
+import android.content.Context
 import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -7,15 +8,29 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.annotation.RequiresApi
 import androidx.fragment.app.Fragment
+import nz.ac.uclive.ajs418.quickfire.MainActivity
 import nz.ac.uclive.ajs418.quickfire.R
 import nz.ac.uclive.ajs418.quickfire.service.BluetoothClientService
 import nz.ac.uclive.ajs418.quickfire.service.BluetoothServiceCallback
+import nz.ac.uclive.ajs418.quickfire.viewmodel.LikeViewModel
+import nz.ac.uclive.ajs418.quickfire.viewmodel.PartyViewModel
+import nz.ac.uclive.ajs418.quickfire.viewmodel.UserViewModel
 
 class ClientPlayFragment : Fragment(), BluetoothServiceCallback {
     private lateinit var bluetoothClientService: BluetoothClientService
+    private lateinit var userViewModel: UserViewModel
+    private lateinit var partyViewModel: PartyViewModel
+    private lateinit var likeViewModel: LikeViewModel
 
     fun setBluetoothClientService(service: BluetoothClientService) {
         this.bluetoothClientService = service
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        userViewModel = (requireActivity() as MainActivity).getUserViewModelInstance()
+        partyViewModel = (requireActivity() as MainActivity).getPartyViewModelInstance()
+        likeViewModel = (requireActivity() as MainActivity).getLikeViewModelInstance()
     }
 
     override fun onCreateView(
--- Quickfire/app/src/main/java/nz/ac/uclive/ajs418/quickfire/fragments/ServerPlayFragment.kt
+++ Quickfire/app/src/main/java/nz/ac/uclive/ajs418/quickfire/fragments/ServerPlayFragment.kt
@@ -1,5 +1,6 @@
 package nz.ac.uclive.ajs418.quickfire.fragments
 
+import android.content.Context
 import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -7,23 +8,29 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.annotation.RequiresApi
 import androidx.fragment.app.Fragment
+import nz.ac.uclive.ajs418.quickfire.MainActivity
 import nz.ac.uclive.ajs418.quickfire.R
 import nz.ac.uclive.ajs418.quickfire.service.BluetoothServerService
 import nz.ac.uclive.ajs418.quickfire.service.BluetoothServiceCallback
+import nz.ac.uclive.ajs418.quickfire.viewmodel.LikeViewModel
+import nz.ac.uclive.ajs418.quickfire.viewmodel.PartyViewModel
+import nz.ac.uclive.ajs418.quickfire.viewmodel.UserViewModel
 
 class ServerPlayFragment : Fragment(), BluetoothServiceCallback {
     private lateinit var bluetoothServerService: BluetoothServerService
+    private lateinit var userViewModel: UserViewModel
+    private lateinit var partyViewModel: PartyViewModel
+    private lateinit var likeViewModel: LikeViewModel
 
     fun setBluetoothServerService(service: BluetoothServerService) {
         this.bluetoothServerService = service
     }
 
-    override fun onDataReceived(data: String) {
-        // Handle the received data here
-    }
-
-    private fun sendData(data: String) {
-        bluetoothServerService.writeData(data)
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        userViewModel = (requireActivity() as MainActivity).getUserViewModelInstance()
+        partyViewModel = (requireActivity() as MainActivity).getPartyViewModelInstance()
+        likeViewModel = (requireActivity() as MainActivity).getLikeViewModelInstance()
     }
 
     override fun onCreateView(
@@ -38,6 +45,14 @@ class ServerPlayFragment : Fragment(), BluetoothServiceCallback {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         bluetoothServerService.setCallback(this)
+    }
+
+    override fun onDataReceived(data: String) {
+        // Handle the received data here
+    }
+
+    private fun sendData(data: String) {
+        bluetoothServerService.writeData(data)
     }
 
 }
